b1775b426a2e14734a1f59eeaeea38667d51996c
TutoBienEtre <92874475+TutoBienEtre@users.noreply.github.com>
2021-10-20 17:26:26 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import './App.css';
+import './data/data.csv';
+import logotuto from './LogoTuto.png'
+import React, { useEffect, useState }  from 'react';
+import { readRemoteFile } from 'react-papaparse';
+import bg from './bg.jpg';
+
+ 
+
+
+let j = require('./data/data.json');
+
+
+const runCallback = (cb) => {
+  return cb();
+};
+
+
+function App() {
+
+  //const [ Data, setData ] = useState(null);
+
+  const [ rows, setRows ] = useState([]);
+
+
+
+  
+
+  useEffect(() => {
+   
+    readRemoteFile('https://docs.google.com/spreadsheets/d/e/2PACX-1vRXi-TcKwaEYai8I5dCjOKlaMJ7AR7E5TCAq72muzObYC-YDHQahJvuvhGLYl3z0A/pub?gid=191056284&single=true&output=csv', {
+      complete: (results) => {
+       // setData(results);
+      const temp = [];
+      const nombreMax = results.data.length;
+      temp.push(
+        <div>
+          <p>{results.data[0][0]}</p>
+          <div class="containerRow">
+             <p className="NumeroTableau">N°</p>
+             <p className="TitreTableau">Nom de l'équipe</p>
+             <p className="ScoresTableau">Points</p>
+           </div>
+         </div>
+        );
+
+
+
+      for (var i = 1; i < nombreMax; i++) {
+
+        temp.push(
+          <div class="containerRow">
+            
+            <p class="rectangleRank">{i}</p>
+            <p class="rectangleName">{results.data[i][0]}</p>
+            <p class="rectangleScore">{results.data[i][1]}</p>
+          </div> 
+          
+        );
+
+        /*temp.push(
+          <p class="rectangleRank">
+            {i}
+            <p class="rectangleName">
+              {results.data[i][0]}
+              <p class="rectangleScore">
+              </p>
+            </p>
+            {results.data[i][1]}
+          </p>
+        );*/
+      }
+
+       setRows(temp);
+      }, 
+    });
+  }, []);
+  /*
+  var AffichageRank = () => {  
+    const temp = [];
+   
+    for (var i = 0; i < nombreMax; i++) {
+      row.push(<p class="rectangleRank">{i + 1}<p class="rectangleName">{Data}<p class="rectangleScore">{j.Data[i].POINTS}</p></p></p>);
+    }
+    return row;
+  };*/
+  
+
+  return (   
+    <div className="App" style={{backgroundImage: `url(${bg})`}}>
+      <img src={logotuto} className="App-logo"/>
+      <h1 className="Titre">Affichage des scores en direct !</h1>
+      <div className="containerColumn">
+        {rows}
+      </div>
+      <a className= 'credits' href="https://fr.freepik.com/vecteurs/fond" style={{color:'#FFFFFF'}}>Fond vecteur créé par coolvector - fr.freepik.com</a>
+     
+   </div>
+  );
+};
+
+export default App ;
+
+
+
